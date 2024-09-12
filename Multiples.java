27d003b7f998603f991c98cd8eaf60a880bd39d4
yasmatopia <missy@itquest.ca>
2024-09-12 14:22:59 -0400

    multiples updated

---
+++ Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples {
+    public static int main(int n, int a, int b) {
+        return (multiples(n, a, b));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int i = 1;
+        int sum = 0;
+
+        while (i < n) {
+            if (i % a == 0 || i % b == 0){
+                sum += 1;
+            }
+            i++;
+        }
+        return sum;
+    }
+}
+
