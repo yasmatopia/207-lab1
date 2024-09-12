b317ff895d5b27339e92046ba02fe981e880600e
yasmatopia <missy@itquest.ca>
2024-09-12 14:54:39 -0400

    multiples updated

--- Multiples.java
+++ Multiples.java
@@ -1,8 +1,11 @@
 public class Multiples {
     public static int main(int n, int a, int b) {
-        return (multiples(n, a, b));
+        return multiples(n, a, b);
     }
 
+    public static int main() {
+        return multiples(1000, 3, 5);
+    }
     public static int multiples(int n, int a, int b) {
         int i = 1;
         int sum = 0;
